4669837a0acd9217564f3daeefdc44b7eeb47668
Khan Takhta <umarkhan314@outlook.com>
2019-07-03 16:52:44 -0500

    test

--- application.py
+++ application.py
@@ -49,21 +49,23 @@ symbols = {
     'USO':'USO'
 }
 
-stock_dfs=[df_eia]
+#stock_dfs=[df_eia]
 
-for name,symbol in symbols.items():
-    r = requests.get(stock_api+symbol+st_apikey)
-    seriesjson= json.loads(r.text)
-    datadict=seriesjson['Time Series (Daily)']
-    df = pd.DataFrame.from_dict(datadict,orient='index')
-    df=df[['4. close']]
-    df.columns=[symbol+'_Closing']
+#for name,symbol in symbols.items():
+ #   r = requests.get(stock_api+symbol+st_apikey)
+  #  seriesjson= json.loads(r.text)
+   # datadict=seriesjson['Time Series (Daily)']
+   # df = pd.DataFrame.from_dict(datadict,orient='index')
+   # df=df[['4. close']]
+   # df.columns=[symbol+'_Closing']
 
-    df.index = pd.to_datetime(df.index)
+   # df.index = pd.to_datetime(df.index)
 
-    stock_dfs.append(df)
+   # stock_dfs.append(df)
 
-consol_df=functools.reduce(lambda x,y: x.join(y,how='outer'),stock_dfs)
+#consol_df=functools.reduce(lambda x,y: x.join(y,how='outer'),stock_dfs)
+#DELTE THE NEXT ASSIGNMENT
+consol_df=[df_eia]
 consol_df=consol_df.loc['2019-01-18':]
 consol_df.fillna(axis=0,method='ffill',inplace=True)
 
